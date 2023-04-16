7a8a98cc21e7a2a5c7899182c0716851bf3a0c25
SamarthTripathi-Design <tripathi.samarth@gmail.com>
2023-04-16 20:37:06 +0530

    Lazyloading chnages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home.jsx";
-import Skill from "./components/Skills/Skill.jsx";
-import Qualification from "./components/Qualification/Qualification.jsx";
-import Contact from "./components/Contact/contact.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+
+const LazySkill = React.lazy(() => import("./components/Skills/Skill.jsx"));
+const LazyQualification = React.lazy(() =>
+  import("./components/Qualification/Qualification.jsx")
+);
+const LazyContact = React.lazy(() =>
+  import("./components/Contact/contact.jsx")
+);
 
 const App = () => {
   return (
     <>
       <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/qualifications" element={<Qualification />} />
-        <Route exact path="/skills" element={<Skill />} />
-        <Route exact path="/contact" element={<Contact />} />
-      </Routes>
+      <Suspense fallback={<p>"Loading..."</p>}>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/qualifications" element={<LazyQualification />} />
+          <Route exact path="/skills" element={<LazySkill />} />
+          <Route exact path="/contact" element={<LazyContact />} />
+        </Routes>
+      </Suspense>
+      <Footer />
     </>
   );
 };
